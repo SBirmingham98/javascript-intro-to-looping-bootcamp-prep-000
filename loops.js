e380e81c634bdfa71d6295a98ea1a36c57aa6549
Sean Birmingham <seannbirmingham98@gmail.com>
2019-08-31 23:02:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
   for(let i = 0; i < 25; i++) {
     if(i === 1) {
-      array.push()
+      array.push('I am 1 strange loop.')
     } else {
-      console.log(`I am ${i} stange loops`)
+      array.push(`I am ${i} stange loops.`)
     }
   }
 }
